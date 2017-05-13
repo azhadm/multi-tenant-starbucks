07f8ca9fba5b8d34191f4e8a0b502a60ad4f4920
khoale88 <lenguyenkhoa1988@gmail.com>
2017-05-13 05:37:00 -0700

    bug fix

--- frontend/helper.py
+++ frontend/helper.py
@@ -1,24 +1,36 @@
 import requests
-from flask import Response
+from flask import Response, json
 
 def forward_request(forward_base_url, request):
     """forward the request to determined url and return a Flask.Response"""
     while forward_base_url[-1] == '/':
         forward_base_url = forward_base_url[:-1]
-    url = forward_base_url+request.path
+
+    if 'Uri' in request.headers:
+        store_uri = '/' + request.headers['Uri']
+        # del request.headers['Uri']
+    else:
+        store_uri = ""
+
+    hds = dict(request.headers)
+    hdddd = {k:v for k, v in hds.iteritems() if k != 'Uri'}
+    # print hdddd
+
+    url = forward_base_url + store_uri + request.path
     data = request.data
     if len(data) == 0:
         data = None
 
-    print(request.headers)
+    # print(request.headers)
     print("Forwarding request to " + url)
     resp = requests.request(method=request.method,
                             url=url,
-                            headers=dict(request.headers),
+                            headers=dict(hdddd),
                             data=data)
-    print(resp.text)
-    print(resp.text)
-    print(resp.text)
+    # print(resp.text)
+    # print(resp.text)
+    # print(resp.text)
+    print resp.text
     if 'Transfer-Encoding' in resp.headers:
         del resp.headers['Transfer-Encoding']
     return Response(headers=dict(resp.headers),
