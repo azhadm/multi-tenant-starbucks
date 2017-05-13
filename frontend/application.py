303c1bbfc2a5c31874de86028cc7cbf78968c555
khoale88 <lenguyenkhoa1988@gmail.com>
2017-05-12 23:06:07 -0700

    minor fix

--- frontend/application.py
+++ frontend/application.py
@@ -1,4 +1,4 @@
-import uuid
+import os, sys
 import requests
 from flask import Flask, Response, redirect, url_for, render_template, json, session, request
 from helper import forward_request
@@ -8,8 +8,11 @@ application = Flask(__name__)
 application.config["SERVER_HOST"] = "0.0.0.0"
 application.config["SERVER_PORT"] = 8080
 
-application.config["KONG_BASE_ADDR"] = 'http://localhost:9090'
-# application.config["KONG_BASE_ADDR"] = 'http://restbucks-api-sanjose-lb-1067926920.us-west-1.elb.amazonaws.com:9090'
+application.config["KONG_BASE_URL"] = os.getenv("KONG_BASE_URL")
+if application.config["KONG_BASE_URL"] is None:
+#     application.config["KONG_BASE_URL"] = 'http://restbucks-api-sanjose-lb-1067926920.us-west-1.elb.amazonaws.com:9090'
+    application.config["KONG_BASE_URL"] = 'http://localhost:9090'
+
 @application.route('/ping', methods=['GET'])
 def ping():
     """for pinging purpose"""
@@ -19,21 +22,21 @@ def ping():
 def get_order(order_id):
     """forward the get all orders request to kong API
     and forward the response to client"""
-    return forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    return forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
 
 @application.route('/starbucks/order/<string:order_id>', methods=['DELETE'])
 def cancel_order(order_id):
     """forward the get all orders request to kong API
     and forward the response to client"""
-    return forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    return forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
 
 @application.route('/starbucks/orders', methods=['GET'])
 def get_all_orders():
     """forward the get all orders request to kong API
     and forward the response to client"""
-    resp = forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    resp = forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
     del resp.headers['Transfer-Encoding']
     return resp
@@ -42,21 +45,21 @@ def get_all_orders():
 def update_order(order_id):
     """forward the place order request to kong API
     and forward the response to client"""
-    return forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    return forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
 
 @application.route('/starbucks/order', methods=['POST'])
 def place_order():
     """forward the place order request to kong API
     and forward the response to client"""
-    return forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    return forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
 
 @application.route('/starbucks/order/<string:order_id>/pay', methods=['POST'])
 def pay_order(order_id):
     """forward the place order request to kong API
     and forward the response to client"""
-    return forward_request(forward_base_url=application.config['KONG_BASE_ADDR'],
+    return forward_request(forward_base_url=application.config['KONG_BASE_URL'],
                            request=request)
 
 
